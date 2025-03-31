4d501fb56e240860369e370cad8b6b3a019e82a4
MaxDevelopsYT <maxdevelopsyt@gmail.com>
2025-03-31 16:20:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
+      name: "Springfield City Roleplay DJ",
+      password: "sfcrp",
       host: "193.226.78.187",
       port:  9372,
       secure: false
